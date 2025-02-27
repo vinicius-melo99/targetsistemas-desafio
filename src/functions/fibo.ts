08f85cb0e4ac3384b87730541b431746438deb8a
Vinicius Melo <viniciusmelo317@gmail.com>
2025-02-27 18:46:08 -0300

    desafios 2, 3, 4 e 5

---
+++ src/functions/fibo.ts
@@ -0,0 +1,17 @@
+export default (numberToCalc: number) => {
+  const fibo = [0];
+
+  for (let i = 0; i <= numberToCalc; i++) {
+    let sum = 0;
+
+    if (i === 0) {
+      fibo.push(1);
+      continue;
+    }
+
+    sum += fibo[i] + fibo[i - 1];
+    fibo.push(sum);
+  }
+
+  return fibo;
+};
---
+++ src/functions/reverseWord.ts
@@ -0,0 +1,12 @@
+export default (word: string) => {
+  const wordArray = [...word.split('')];
+  console.log(wordArray);
+
+  const reversedWord = [];
+
+  for (let i = wordArray.length; i >= 0; i--) {
+    reversedWord.push(wordArray[i]);
+  }
+
+  return reversedWord.join('');
+};
